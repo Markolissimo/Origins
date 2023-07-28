7f99f7538eb1f78260a1983edf46ca8e73dfc394
Danylo Musiiaka <dollarone98@gmail.com>
2023-07-28 16:48:44 +0300

    added song page

--- static/js/mainpage.js
+++ static/js/mainpage.js
@@ -5,3 +5,22 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 }
+
+
+const scrollButton = document.getElementById('scrollButton');
+scrollButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' 
+    });
+});
+
+scrollButton.style.display = 'none';
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 180) {
+        scrollButton.style.display = 'block';
+    } else {
+        scrollButton.style.display = 'none';
+    }
+});
