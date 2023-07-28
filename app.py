5949ffdd0cb7ca7e22886e1cd53b441ff67991af
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-07-28 21:58:28 +0300

    Add connect to spotify API and search song

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from config import Config
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
+client_credentials = SpotifyClientCredentials(Config.SPOTIFY_CLIENT_ID,Config.SPOTIFY_CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager =client_credentials)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET','POST'])
 def home():
     return render_template('mainpage.html')
 
 
 @app.post('/song-info')
 def song_info():
-    return;
+    song_name = request.form.get('song_name')
+    song = sp.search(q=song_name,limit = 1)
+    print(song)
+    return "OK", 200
 
 
 if __name__=='__main__':
