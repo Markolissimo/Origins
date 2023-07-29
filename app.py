0388790c4d0c476eb83a2ff25ce076773c1a94bc
Markolissimo <78735800+Markolissimo@users.noreply.github.com>
2023-07-29 04:58:57 +0300

    # app.py Song_info function edited. Now it makes a request to Spotify for track_info(song_name, artist, release_date, url and cover_image) as well as it gets some similar tracks to original with same data (song_name, artist, etc.) Returns: a render_template('song.html', ...), which creates a song.html page with original song info(except description, youtube url and release date - should be added)
    # config.py
    Nothing but some extra spaces were added
    
    # song.html
    Hardcoded values in original-song in about-image, url(for spotify), h2 and h5 were replaced with dynamic ones.
    Buttons were replaced with anchors (<a>) due to button's limitations for HTTP requests.
    Was added a <script> for handling HTTP requests. ChatGPT generated, I don't give a shit what is it doing at all, but is doing great:)
    
    # song.css
    Buttons were replaced with link-logo(class of anchors), despite they have the same characteristics.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from config import Config
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -6,21 +6,40 @@ from spotipy.oauth2 import SpotifyClientCredentials
 app = Flask(__name__)
 app.config.from_object(Config)
 
-client_credentials = SpotifyClientCredentials(Config.SPOTIFY_CLIENT_ID,Config.SPOTIFY_CLIENT_SECRET)
-sp = spotipy.Spotify(client_credentials_manager =client_credentials)
+client_credentials = SpotifyClientCredentials(Config.SPOTIFY_CLIENT_ID, Config.SPOTIFY_CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials)
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('mainpage.html')
 
-
-@app.post('/song-info')
+@app.route('/song-info', methods=['POST'])
 def song_info():
     song_name = request.form.get('song_name')
-    song = sp.search(q=song_name,limit = 1)
-    print(song)
-    return "OK", 200
+    result = sp.search(q=song_name, limit=10)
 
+    if result and 'tracks' in result and 'items' in result['tracks']:
+        original_tracks = []
+        similar_songs = []
 
-if __name__=='__main__':
-    app.run(debug= True)
+        for track in result['tracks']['items']:
+            track_info = {
+                'song_name': track['name'],
+                'artist': track['artists'][0]['name'],
+                'release_date': track['album']['release_date'],
+                'url': track['external_urls']['spotify'],
+                'cover_image': track['album']['images'][0]['url']
+            }
+
+            if track_info not in original_tracks:
+                original_tracks.append(track_info)
+            else:
+                similar_songs.append(track_info)
+
+        if original_tracks:
+            return render_template('song.html', original_track=original_tracks[0], similar_songs=similar_songs)
+
+    return jsonify({'error': 'Song not found'}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
